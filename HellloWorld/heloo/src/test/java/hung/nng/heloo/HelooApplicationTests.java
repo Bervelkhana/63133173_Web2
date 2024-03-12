ca856239f2db7cb2b16a655ee5da47b6dea23124
Hưng Nguyễn <hunggianguyenZX@gmail.com>
2024-03-12 15:05:42 +0700

    update

---
+++ HellloWorld/heloo/src/test/java/hung/nng/heloo/HelooApplicationTests.java
@@ -0,0 +1,13 @@
+package hung.nng.heloo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
