413c6f0b41d9a25964f9e1a21f6d68ab51e19678
Hưng Nguyễn <hunggianguyenZX@gmail.com>
2024-04-16 15:12:49 +0700

    update

---
+++ Project/project/src/main/java/ht/project/Controllers/HomeControllers.java
@@ -0,0 +1,12 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeControllers {
+
+    @GetMapping("/home")
+    public String home() {
+        
+        return "index"; 
+    }
+}
