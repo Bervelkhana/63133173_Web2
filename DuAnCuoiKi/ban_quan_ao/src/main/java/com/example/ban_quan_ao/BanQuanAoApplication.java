7667ff638d4fb0905591e2c989dc1af2d17cdab5
Hưng Nguyễn <hunggianguyenZX@gmail.com>
2024-04-16 16:09:51 +0700

    update

--- DuAnCuoiKi/ban_quan_ao/src/main/java/com/example/ban_quan_ao/BanQuanAoApplication.java
+++ DuAnCuoiKi/ban_quan_ao/src/main/java/com/example/ban_quan_ao/BanQuanAoApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.ban_quan_ao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class BanQuanAoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(BanQuanAoApplication.class, args);
 	}
 
 }
